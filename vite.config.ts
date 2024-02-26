5a0bf1b9e009a3cb6f9f18dacea599ff3d74ec58
mathyasp <mathyastp@gmail.com>
2024-02-26 10:33:33 -0500

    changed to a different framework

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-});
+  plugins: [react()],
+})
