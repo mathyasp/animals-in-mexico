06c1f4664912d0767b9319a923b9f59b8bb64dc1
mathyasp <mathyastp@gmail.com>
2024-02-27 11:34:39 -0500

    created Header component and added more content

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Animals In Mexico</h1>
+      <h2 className="Header-Subtitle">
+        Collection of different animals I saw in Puerto Vallarta, Mexico
+      </h2>
+      <p>February 2024</p>
+    </div>
+  )
+}
+
+export default Header;
