4ff0715c0fb24cabfd0e6c94e605d2da78046d12
mathyasp <mathyastp@gmail.com>
2024-11-19 19:31:28 -0500

    Update the styles on the animal list page

--- src/components/AnimalList/AnimalList.tsx
+++ src/components/AnimalList/AnimalList.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { AnimalType } from '../../types';
 import englishDataWithIds from '../../../animalDataEnglish';
 import spanishDataWithIds from '../../../animalDataSpanish';
-// import Animal from '../Animal';
+
 interface AnimalProps {
   isSpanish: boolean;
 }
@@ -14,11 +14,12 @@ function AnimalList({ isSpanish }: AnimalProps) {
   return (
     <div className='AnimalList'>
       {data.map((animal: AnimalType) => (
-        <Link to={`/animals-in-mexico/animal/${animal.id}`} key={animal.id}>
+        <Link to={`/animals-in-mexico/animal/${animal.id}`} key={animal.id} className='card'>
           <div className='AnimalList-animal'>
-            <h3>{animal.name}</h3>
             {animal.img && <img src={`/animals-in-mexico/images/${animal.img}`} alt={animal.name} />}
             {animal.vid && <video src={`/animals-in-mexico/videos/${animal.vid}`} controls muted loop />}
+            <h3>{animal.name}</h3>
+            <p>{animal.desc}</p>
           </div>
         </Link>
       ))}
